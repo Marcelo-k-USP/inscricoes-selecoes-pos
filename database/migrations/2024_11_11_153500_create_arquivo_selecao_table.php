54e087dfd96ec9fc021b3cfcea27bad5308547e3
Marcelo-k-USP <makf00@gmail.com>
2024-11-11 16:25:15 -0300

    criação de tabelas de arquivos

---
+++ database/migrations/2024_11_11_153500_create_arquivo_selecao_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArquivoSelecaoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('arquivo_selecao', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('arquivo_id')->constrained('arquivos')->onDelete('cascade');
+            $table->foreignId('selecao_id')->constrained('selecoes')->onDelete('cascade');
+
+            $table->string('tipo', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('arquivo_selecao');
+    }
+}
