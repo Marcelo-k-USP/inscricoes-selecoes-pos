456c7bc1da80c1e4dfc8cfca23385a12cf4c6e6e
Marcelo-k-USP <makf00@gmail.com>
2024-12-03 15:37:57 -0300

    função consultar_cep em functions.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -34,3 +34,33 @@ function mudarCampoInputTextarea(campo) {
     });
   }
 }
+
+function validar_cpf(cpf)
+{
+  cpf = cpf.replace(/\./g, '').replace('-', '');
+  if (cpf.length != 11)
+    return false;
+
+  var resto;
+  var soma;
+
+  soma = 0;
+  for (var i = 1; i <= 9; i++)
+    soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+  resto = (soma * 10) % 11;
+  if ((resto == 10) || (resto == 11))
+    resto = 0;
+  if (resto !== parseInt(cpf.substring(9, 10)))
+    return false;
+
+  soma = 0;
+  for (var i = 1; i <= 10; i++)
+    soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+  resto = (soma * 10) % 11;
+  if ((resto == 10) || (resto == 11))
+    resto = 0;
+  if (resto !== parseInt(cpf.substring(10, 11)))
+    return false;
+
+  return true;
+}
--- resources/views/inscricoes/show/card-principal.blade.php
+++ resources/views/inscricoes/show/card-principal.blade.php
@@ -44,6 +44,7 @@
 
 @section('javascripts_bottom')
 @parent
+  <script src="js/functions.js"></script>
   <script>
     $(document).ready(function() {
 
@@ -131,36 +132,6 @@
               window.alert(xhr.responseText);
           }
       });
-    }
-
-    function validar_cpf(cpf)
-    {
-      cpf = cpf.replace(/\./g, '').replace('-', '');
-      if (cpf.length != 11)
-        return false;
-
-      var resto;
-      var soma;
-
-      soma = 0;
-      for (var i = 1; i <= 9; i++)
-        soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
-      resto = (soma * 10) % 11;
-      if ((resto == 10) || (resto == 11))
-        resto = 0;
-      if (resto !== parseInt(cpf.substring(9, 10)))
-        return false;
-
-      soma = 0;
-      for (var i = 1; i <= 10; i++)
-        soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
-      resto = (soma * 10) % 11;
-      if ((resto == 10) || (resto == 11))
-        resto = 0;
-      if (resto !== parseInt(cpf.substring(10, 11)))
-        return false;
-
-      return true;
     }
   </script>
 @endsection
