9333f841650077c9c3e51c004a0d955fe919c50e
Marcelo-k-USP <makf00@gmail.com>
2024-12-05 16:23:51 -0300

    colocando o ícone ocultar/mostrar senha dentro do campo de senha

--- resources/views/common/list-table-form-password.blade.php
+++ resources/views/common/list-table-form-password.blade.php
@@ -6,9 +6,10 @@
   <div class="col-sm-3" style="position: relative;">
     {{ html()->input('password', $col['name'])
       ->value(old($col['name'], $modo == 'edit' ? $modelo->{$col['name']} : ''))
-      ->class('form-control w-100')
+      ->class('form-control')
+      ->style('width: 100%; padding-right: 30px;')
     }}
-    <a href="javascript:void(0);" onclick="toggle_senha()" style="position: absolute; right: -20px; top: 20%; text-decoration: none;">
+    <a href="javascript:void(0);" onclick="toggle_senha()" style="position: absolute; right: 24px; top: 20%; text-decoration: none;">
       <img src="/icons/view.png" id="toggle_icon" style="width: 20px; height: 20px;">
     </a>
   </div>
