e8865418427927060558d3e6863a4b5cf67f362f
Marcelo-k-USP <makf00@gmail.com>
2024-12-05 16:21:50 -0300

    botão oculta/mostra senha na tela de login de usuário local

--- resources/views/localusers/login.blade.php
+++ resources/views/localusers/login.blade.php
@@ -12,9 +12,19 @@
         <label for="email" class="sr-only">E-mail</label>
         <input class="form-control" type="text" name="email" id="email" placeholder="E-mail" autofocus>
         <label for="password" class="sr-only">Senha</label>
-        <input class="form-control mb-4" type="password" name="password" id="password" placeholder="Senha">
+        <div style="position: relative;">
+          <input class="form-control mb-4" style="width: 100%; padding-right: 30px;" type="password" name="password" id="password" placeholder="Senha">
+          <a href="javascript:void(0);" onclick="toggle_senha()">
+            <img src="/icons/view.png" id="toggle_icon" style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); cursor: pointer; width: 20px; height: 20px;">
+          </a>
+        </div>
         <button type="submit" class="btn btn-lg btn-success btn-block">Login</button>
       </form>
     </div>
   @endauth
+@endsection
+
+@section('javascripts_bottom')
+@parent
+  <script src="js/functions.js"></script>
 @endsection
