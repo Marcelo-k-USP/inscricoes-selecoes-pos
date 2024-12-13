f5e7cbdff8932e0b09239985c13085dd9a729bf7
Marcelo-k-USP <makf00@gmail.com>
2024-12-13 15:30:20 -0300

    melhorias nos textos informativos

--- resources/views/emails/inscricao_nova.blade.php
+++ resources/views/emails/inscricao_nova.blade.php
@@ -1,6 +1,7 @@
 Olá {{ $autor->name }},<br />
 <br />
 Você realizou sua inscrição com sucesso no processo seletivo {{ $inscricao->selecao->nome }}.<br />
+Não deixe de pagar o boleto em anexo, e também não deixe de enviar todos os arquivos requeridos na inscrição.<br />
 <br />
 {!! $arquivo_erro !!}<br />
 @include('emails.rodape')
