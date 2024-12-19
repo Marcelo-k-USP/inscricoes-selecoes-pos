aae4dfdc143be84a86374ca1bc3f414489481c9f
Marcelo-k-USP <makf00@gmail.com>
2024-12-19 16:32:05 -0300

    mais tempo de exibição em mensagens flash com mais de uma linha

--- resources/views/messages/flash.blade.php
+++ resources/views/messages/flash.blade.php
@@ -1,12 +1,9 @@
 <div class="flash-message fixed-bottom w-75 ml-auto mr-auto">
   @foreach (['danger', 'warning', 'success', 'info'] as $msg)
-    @if (Session::has('alert-' . $msg))
-      <p class="alert alert-{{ $msg }}">{!! Session::get('alert-' . $msg) !!}
+    @if(Session::has('alert-' . $msg))
+      <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }}
         <a href="#" class="close" data-dismiss="alert" aria-label="fechar">&times;</a>
       </p>
-      @php
-        Session::forget('alert-' . $msg);    // tendo exibido o alert, remove-o da sessão para que ele não seja reexibido por exemplo com um Ctrl+F5
-      @endphp
     @endif
   @endforeach
 </div>
@@ -14,9 +11,10 @@
 @section('javascripts_bottom')
 @parent
   <script type="text/javascript">
-    $(function() {
-      $(".flash-message").fadeTo(5000, 500).slideUp(500, function() {
-        $(".flash-message").slideUp(500);
+    $('.flash-message').each(function () {
+      var duration = $(this).html().includes('<br />') ? 10000 : 5000;
+      $(this).fadeTo(duration, 500).slideUp(500, function() {
+        $('.flash-message').slideUp(500);
       });
     });
   </script>
