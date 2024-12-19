1115928a21b2e7de0071e1e4808645779d0a0543
Marcelo-k-USP <makf00@gmail.com>
2024-12-19 16:45:02 -0300

    acerto no tempo do flash

--- resources/views/messages/flash.blade.php
+++ resources/views/messages/flash.blade.php
@@ -1,7 +1,7 @@
 <div class="flash-message fixed-bottom w-75 ml-auto mr-auto">
   @foreach (['danger', 'warning', 'success', 'info'] as $msg)
-    @if(Session::has('alert-' . $msg))
-      <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }}
+    @if (Session::has('alert-' . $msg))
+      <p class="alert alert-{{ $msg }}">{!! Session::get('alert-' . $msg) !!}
         <a href="#" class="close" data-dismiss="alert" aria-label="fechar">&times;</a>
       </p>
     @endif
@@ -12,7 +12,7 @@
 @parent
   <script type="text/javascript">
     $('.flash-message').each(function () {
-      var duration = $(this).html().includes('<br />') ? 10000 : 5000;
+      var duration = $(this).html().includes('<br>') ? 10000 : 5000;
       $(this).fadeTo(duration, 500).slideUp(500, function() {
         $('.flash-message').slideUp(500);
       });
