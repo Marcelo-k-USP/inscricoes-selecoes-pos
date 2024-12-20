8919003c9a7417966e4714305fd36cc13a5699b3
Marcelo-k-USP <makf00@gmail.com>
2024-12-20 08:15:04 -0300

    acerta posicionamento do botão de fechar das flash messages

--- resources/views/messages/flash.blade.php
+++ resources/views/messages/flash.blade.php
@@ -10,6 +10,27 @@
 
 @section('javascripts_bottom')
 @parent
+  <style>
+    .flash-message {
+      position: fixed;
+      bottom: 0;
+      width: 75%;
+      left: 50%;
+      transform: translateX(-50%);
+      z-index: 1050;
+    }
+    .flash-message .alert {
+      position: relative;
+      padding-right: 35px;  /* Espaço para o botão de fechar */
+      margin-bottom: 10px;  /* Espaço abaixo do alerta */
+    }
+    .flash-message .close {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      z-index: 2;
+    }
+  </style>
   <script type="text/javascript">
     $('.flash-message').each(function () {
       var duration = $(this).html().includes('<br>') ? 10000 : 5000;
