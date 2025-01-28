d258f141e13766415ba0ad50027d3b87274f6733
Marcelo-k-USP <makf00@gmail.com>
2025-01-28 13:44:37 -0300

    melhorias na comunicação com o candidato

--- resources/views/emails/inscricao_aprovacao.blade.php
+++ resources/views/emails/inscricao_aprovacao.blade.php
@@ -1,6 +1,5 @@
 Olá {{ $user->name }},<br />
 <br />
 Sua inscrição para o processo seletivo {{ $inscricao->selecao->nome }} foi aceita.<br />
-E agora?<br />
 <br />
 @include('emails.rodape')
--- resources/views/emails/inscricao_rejeicao.blade.php
+++ resources/views/emails/inscricao_rejeicao.blade.php
@@ -1,6 +1,5 @@
 Olá {{ $user->name }},<br />
 <br />
-Sua inscrição para o processo seletivo {{ $inscricao->selecao->nome }} foi rejeitada.<br />
-E agora?<br />
+Lamentamos, mas sua inscrição para o processo seletivo {{ $inscricao->selecao->nome }} foi rejeitada.<br />
 <br />
 @include('emails.rodape')
