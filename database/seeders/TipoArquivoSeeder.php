256cc28f1948ffd9fb900a73f68fc07230711bda
Marcelo-k-USP <makf00@gmail.com>
2025-02-04 15:52:44 -0300

    cadastrados tipos de arquivo conforme níveis

--- database/seeders/TipoArquivoSeeder.php
+++ database/seeders/TipoArquivoSeeder.php
@@ -49,12 +49,37 @@ class TipoArquivoSeeder extends Seeder
             ],
             [
                 'classe_nome' => 'Inscrições',
-                'nome' => 'Comprovação de Proficiência em Língua Estrangeira',
+                'nome' => 'Histórico Escolar da Graduação',
                 'obrigatorio' => true,
             ],
             [
                 'classe_nome' => 'Inscrições',
-                'nome' => 'Histórico Escolar e Diploma de Gradução',
+                'nome' => 'Histórico Escolar do Mestrado',
+                'obrigatorio' => true,
+            ],
+            [
+                'classe_nome' => 'Inscrições',
+                'nome' => 'Diploma da Graduação',
+                'obrigatorio' => true,
+            ],
+            [
+                'classe_nome' => 'Inscrições',
+                'nome' => 'Diploma do Mestrado',
+                'obrigatorio' => true,
+            ],
+            [
+                'classe_nome' => 'Inscrições',
+                'nome' => 'Dissertação do Mestrado',
+                'obrigatorio' => true,
+            ],
+            [
+                'classe_nome' => 'Inscrições',
+                'nome' => 'Comprovação de Proficiência em uma Língua Estrangeira',
+                'obrigatorio' => true,
+            ],
+            [
+                'classe_nome' => 'Inscrições',
+                'nome' => 'Comprovação de Publicação de um Artigo em Revista Científica',
                 'obrigatorio' => true,
             ],
             [
@@ -87,10 +112,38 @@ class TipoArquivoSeeder extends Seeder
         $nivel_id_MESTRADO = Nivel::where('nome', 'Mestrado')->first()->id;
         $nivel_id_DOUTORADO = Nivel::where('nome', 'Doutorado')->first()->id;
         $nivel_id_DOUTORADODIRETO = Nivel::where('nome', 'Doutorado Direto')->first()->id;
-        foreach (TipoArquivo::where('classe_nome', 'Inscrições')->get() as $tipoarquivo) {
-            $tipoarquivo->niveis()->attach($nivel_id_MESTRADO);
-            $tipoarquivo->niveis()->attach($nivel_id_DOUTORADO);
-            $tipoarquivo->niveis()->attach($nivel_id_DOUTORADODIRETO);
-        }
+        $tipoarquivo_DOCUMENTOCOMFOTO = TipoArquivo::where('nome', 'Documento com Foto')->first();
+        $tipoarquivo_DOCUMENTOCOMFOTO->niveis()->attach($nivel_id_MESTRADO);
+        $tipoarquivo_DOCUMENTOCOMFOTO->niveis()->attach($nivel_id_DOUTORADO);
+        $tipoarquivo_DOCUMENTOCOMFOTO->niveis()->attach($nivel_id_DOUTORADODIRETO);
+        $tipoarquivo_HISTORICOESCOLARDAGRADUACAO = TipoArquivo::where('nome', 'Histórico Escolar da Graduação')->first();
+        $tipoarquivo_HISTORICOESCOLARDAGRADUACAO->niveis()->attach($nivel_id_MESTRADO);
+        $tipoarquivo_HISTORICOESCOLARDAGRADUACAO->niveis()->attach($nivel_id_DOUTORADO);
+        $tipoarquivo_HISTORICOESCOLARDAGRADUACAO->niveis()->attach($nivel_id_DOUTORADODIRETO);
+        $tipoarquivo_HISTORICOESCOLARDOMESTRADO = TipoArquivo::where('nome', 'Histórico Escolar do Mestrado')->first();
+        $tipoarquivo_HISTORICOESCOLARDOMESTRADO->niveis()->attach($nivel_id_DOUTORADO);
+        $tipoarquivo_HISTORICOESCOLARDOMESTRADO->niveis()->attach($nivel_id_DOUTORADODIRETO);
+        $tipoarquivo_DIPLOMADAGRADUACAO = TipoArquivo::where('nome', 'Diploma da Graduação')->first();
+        $tipoarquivo_DIPLOMADAGRADUACAO->niveis()->attach($nivel_id_MESTRADO);
+        $tipoarquivo_DIPLOMADAGRADUACAO->niveis()->attach($nivel_id_DOUTORADO);
+        $tipoarquivo_DIPLOMADAGRADUACAO->niveis()->attach($nivel_id_DOUTORADODIRETO);
+        $tipoarquivo_DIPLOMADOMESTRADO = TipoArquivo::where('nome', 'Diploma do Mestrado')->first();
+        $tipoarquivo_DIPLOMADOMESTRADO->niveis()->attach($nivel_id_DOUTORADO);
+        $tipoarquivo_DIPLOMADOMESTRADO->niveis()->attach($nivel_id_DOUTORADODIRETO);
+        $tipoarquivo_DISSERTACAODOMESTRADO = TipoArquivo::where('nome', 'Dissertação do Mestrado')->first();
+        $tipoarquivo_DISSERTACAODOMESTRADO->niveis()->attach($nivel_id_DOUTORADO);
+        $tipoarquivo_DISSERTACAODOMESTRADO->niveis()->attach($nivel_id_DOUTORADODIRETO);
+        $tipoarquivo_COMPROVACAODEPROFICIENCIAEMUMALINGUAESTRANGEIRA = TipoArquivo::where('nome', 'Comprovação de Proficiência em uma Língua Estrangeira')->first();
+        $tipoarquivo_COMPROVACAODEPROFICIENCIAEMUMALINGUAESTRANGEIRA->niveis()->attach($nivel_id_MESTRADO);
+        $tipoarquivo_COMPROVACAODEPROFICIENCIAEMUMALINGUAESTRANGEIRA->niveis()->attach($nivel_id_DOUTORADO);
+        $tipoarquivo_COMPROVACAODEPROFICIENCIAEMUMALINGUAESTRANGEIRA->niveis()->attach($nivel_id_DOUTORADODIRETO);
+        $tipoarquivo_COMPROVACAODEPUBLICACAODEUMARTIGOEMREVISTACIENTIFICA = TipoArquivo::where('nome', 'Comprovação de Publicação de um Artigo em Revista Científica')->first();
+        $tipoarquivo_COMPROVACAODEPUBLICACAODEUMARTIGOEMREVISTACIENTIFICA->niveis()->attach($nivel_id_DOUTORADO);
+        $tipoarquivo_COMPROVACAODEPUBLICACAODEUMARTIGOEMREVISTACIENTIFICA->niveis()->attach($nivel_id_DOUTORADODIRETO);
+        $tipoarquivo_COMPROVACAODEPUBLICACAODENOMINIMODOISARTIGOSEMREVISTACIENTIFICA = TipoArquivo::where('nome', 'Comprovação de Publicação de no Mínimo 2 Artigos em Revista Científica')->first();
+        $tipoarquivo_BOLETOSDEPAGAMENTODAINSCRICAO = TipoArquivo::where('nome', 'Boleto(s) de Pagamento da Inscrição')->first();
+        $tipoarquivo_BOLETOSDEPAGAMENTODAINSCRICAO->niveis()->attach($nivel_id_MESTRADO);
+        $tipoarquivo_BOLETOSDEPAGAMENTODAINSCRICAO->niveis()->attach($nivel_id_DOUTORADO);
+        $tipoarquivo_BOLETOSDEPAGAMENTODAINSCRICAO->niveis()->attach($nivel_id_DOUTORADODIRETO);
     }
 }
