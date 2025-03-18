2c3831082d64d0a17e23d5b82f9a3c2fc1094e94
Marcelo-k-USP <makf00@gmail.com>
2025-03-18 14:46:08 -0300

    limpeza de dados separada num job

--- app/Http/Controllers/LimpezaDadosController.php
+++ app/Http/Controllers/LimpezaDadosController.php
@@ -3,14 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LimpezaDadosRequest;
-use App\Models\Arquivo;
-use App\Models\Inscricao;
-use App\Models\Selecao;
-use App\Models\SolicitacaoIsencaoTaxa;
+use App\Jobs\LimpaDados;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\URL;
@@ -39,48 +34,8 @@ class LimpezaDadosController extends Controller
         if ($validator->fails())
             return back()->withErrors($validator)->withInput();
 
-        $data_limite = Carbon::createFromFormat('d/m/Y', $request->data_limite);
-
-        // transaction para não ter problema de inconsistência do DB
-        $db_transaction = DB::transaction(function () use ($data_limite) {
-
-            // apaga todas as inscrições gravadas no banco de dados até essa data
-            foreach (Inscricao::where('created_at', '<=', $data_limite)->get() as $inscricao) {
-                $inscricao->arquivos()->detach();
-                $inscricao->pessoas()->detach();
-                $inscricao->delete();
-            }
-
-            // apaga todas as solicitações de isenção de taxa gravadas no banco de dados até essa data
-            foreach (SolicitacaoIsencaoTaxa::where('created_at', '<=', $data_limite)->get() as $solicitacaoisencaotaxa) {
-                $solicitacaoisencaotaxa->arquivos()->detach();
-                $solicitacaoisencaotaxa->pessoas()->detach();
-                $solicitacaoisencaotaxa->delete();
-            }
-
-            // apaga todas as seleções gravadas no banco de dados até essa data
-            foreach (Selecao::where('created_at', '<=', $data_limite)->get() as $selecao) {
-                $selecao->arquivos()->detach();
-                $selecao->disciplinas()->detach();
-                $selecao->linhaspesquisa()->detach();
-                $selecao->motivosisencaotaxa()->detach();
-                $selecao->niveislinhaspesquisa()->detach();
-                $selecao->tiposarquivo()->detach();
-                $selecao->delete();
-            }
-
-            // apaga todos os arquivos gravados no banco de dados até essa data
-            foreach (Arquivo::where('created_at', '<=', $data_limite)->get() as $arquivo)
-                $arquivo->delete();
-        });
-
-        // apaga todos os arquivos gravados no servidor até essa data
-        $pasta_base = storage_path('app/arquivos');
-        if (File::exists($pasta_base))
-            foreach (File::directories($pasta_base) as $subpasta)
-                foreach (File::files($subpasta) as $arquivo)
-                    if (Carbon::createFromTimestamp(File::lastModified($arquivo))->lte($data_limite))
-                        File::delete($arquivo);
+        \Illuminate\Support\Facades\Log::info('$request->data_limite: ' . $request->data_limite);
+        LimpaDados::dispatch(Carbon::createFromFormat('d/m/Y', $request->data_limite));
 
         $request->session()->flash('alert-success', 'Operação realizada com sucesso');
         \UspTheme::activeUrl('limpezadados');
---
+++ app/Jobs/LimpaDados.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Arquivo;
+use App\Models\Inscricao;
+use App\Models\Selecao;
+use App\Models\SolicitacaoIsencaoTaxa;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
+class LimpaDados implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $data_limite;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($data_limite)
+    {
+        $this->data_limite = $data_limite;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $data_limite = $this->data_limite;
+
+        // transaction para não ter problema de inconsistência do DB
+        $db_transaction = DB::transaction(function () use ($data_limite) {
+
+            // apaga todas as inscrições gravadas no banco de dados até essa data
+            foreach (Inscricao::where('created_at', '<=', $data_limite)->get() as $inscricao) {
+                $inscricao->arquivos()->detach();
+                $inscricao->pessoas()->detach();
+                $inscricao->delete();
+            }
+
+            // apaga todas as solicitações de isenção de taxa gravadas no banco de dados até essa data
+            foreach (SolicitacaoIsencaoTaxa::where('created_at', '<=', $data_limite)->get() as $solicitacaoisencaotaxa) {
+                $solicitacaoisencaotaxa->arquivos()->detach();
+                $solicitacaoisencaotaxa->pessoas()->detach();
+                $solicitacaoisencaotaxa->delete();
+            }
+
+            // apaga todas as seleções gravadas no banco de dados até essa data
+            foreach (Selecao::where('created_at', '<=', $data_limite)->get() as $selecao) {
+                $selecao->arquivos()->detach();
+                $selecao->disciplinas()->detach();
+                $selecao->linhaspesquisa()->detach();
+                $selecao->motivosisencaotaxa()->detach();
+                $selecao->niveislinhaspesquisa()->detach();
+                $selecao->tiposarquivo()->detach();
+                $selecao->delete();
+            }
+
+            // apaga todos os arquivos gravados no banco de dados até essa data
+            foreach (Arquivo::where('created_at', '<=', $data_limite)->get() as $arquivo)
+                $arquivo->delete();
+        });
+
+        // apaga todos os arquivos gravados no servidor até essa data
+        $pasta_base = storage_path('app/arquivos');
+        if (File::exists($pasta_base))
+            foreach (File::directories($pasta_base) as $subpasta)
+                foreach (File::files($subpasta) as $arquivo)
+                    if (Carbon::createFromTimestamp(File::lastModified($arquivo))->lte($data_limite))
+                        File::delete($arquivo);
+    }
+}
