3551ac171a1c2e90c9cad4660863279836e3b607
Marcelo-k-USP <makf00@gmail.com>
2025-09-09 08:27:40 -0300

    melhorias na maneira como evitamos duplicação de partials incluídas

---
+++ resources/views/partials/scripts-arquivos.blade.php
@@ -0,0 +1,6 @@
+@once
+  <script type="text/javascript">
+    var max_upload_size = {{ $max_upload_size }};    // esta variável precisa ser definida aqui para que fique disponível no arquivos.js
+  </script>
+  <script src="js/arquivos.js"></script>
+@endonce
