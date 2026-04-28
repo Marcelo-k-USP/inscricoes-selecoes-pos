da370389751d70e904767bae02e183349d0fff1b
João Pedro de Toledo <jpedrotoledo@usp.br>
2026-04-28 10:05:11 -0300

    pula o programa caso nao encontre

--- database/seeders/FuncaoSeeder.php
+++ database/seeders/FuncaoSeeder.php
@@ -16,34 +16,40 @@ class FuncaoSeeder extends Seeder
      */
 
     /**
-     * Por enquanto só preenche automaticamente os Docenses do Programa, ainda não há criação automática para:
+     * Por enquanto só preenche automaticamente os Docentes do Programa, ainda não há criação automática para:
      * a) Secretários(as) do Programa
      * b) Coordenadores do Programa
      * c) Serviço de Pós-Graduação
      * d) Coordenadores da Pós-Graduação
     */
-    public function run()
+   public function run()
     {
         $programas = Posgraduacao::programas();
+        
         foreach($programas as $programa)
-            {
-                $programaSistema = Programa::where('nome', 'LIKE', "{$programa['nomcur']}%")->first();
-                $docentes = Posgraduacao::orientadores($programa['codare']);
-                foreach($docentes as $docente)
-                {
-                    $user = User::findOrCreateFromReplicado($docente['codpes']);
-                    if ($user) {
-                        // Verifica se o usuário já tem essa função nesse programa específico
-                        $jaAssociado = $user->programas()
-                            ->where('programa_id', $programaSistema->id ?? null)
-                            ->wherePivot('funcao', 'Docentes do Programa')
-                            ->exists();
+        {
+            $programaSistema = Programa::where('nome', 'LIKE', "{$programa['nomcur']}%")->first();
+            if (!$programaSistema) {
+                continue; 
+            }
 
-                        if (!$jaAssociado) {
-                            $user->associarProgramaFuncao($programaSistema['nome'] ?? null, 'Docentes do Programa');
-                        }
+            $docentes = Posgraduacao::orientadores($programa['codare']);
+            
+            foreach($docentes as $docente)
+            {
+                $user = User::findOrCreateFromReplicado($docente['codpes']);
+                
+                if ($user) {
+                    $jaAssociado = $user->programas()
+                        ->where('programa_id', $programaSistema->id)
+                        ->wherePivot('funcao', 'Docentes do Programa')
+                        ->exists();
+
+                    if (!$jaAssociado) {
+                        $user->associarProgramaFuncao($programaSistema->nome, 'Docentes do Programa');
                     }
                 }
             }
-     }
+        }
+    }
 }
